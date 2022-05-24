078875df24aca6a9281afd30f67017851a2896de
Abhishek-Kamble <abhishek.kamble@walchandsangli.ac.in>
2022-05-24 08:43:23 +0530

    Minor Changes

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,7 +6,7 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
+          "Computer Science Enthusiast",
           "Back-end Developer",
           "DSA Enthusiast",
         ],
